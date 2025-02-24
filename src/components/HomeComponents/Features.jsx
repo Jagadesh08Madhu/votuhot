7b22f0f4c37f7e38ca8e83575c3681cc678d6f0f
Jagadesh08Madhu <jagadesh2001kavi@gmail.com>
2025-02-24 16:21:22 +0530

    changes

--- src/components/HomeComponents/Features.jsx
+++ src/components/HomeComponents/Features.jsx
@@ -8,37 +8,37 @@ import { motion } from "framer-motion";
 export default function Features() {
   const FeaturesContent = [
     {
-      icon: <LuScaling className='text-6xl text-[#7049C3]' />,
+      icon: <LuScaling className='text-5xl text-[#7049C3]' />,
       heading: "Flexible Resource Scaling",
       content: `Easily adjust server capacity to handle sudden traffic increases or planned growth using Dynamic Resource Allocation. 
       This ensures your website or application remains fast and responsive at all times.`
     },
     {
-      icon: <MdOutlineSpeed className='text-6xl text-[#7049C3]' />,
+      icon: <MdOutlineSpeed className='text-5xl text-[#7049C3]' />,
       heading: "Optimized Speed and Efficiency",
       content: `Boost network and storage performance with High-Speed SSDs and advanced caching techniques. 
       This results in quicker load times and smoother user experiences.`
     },
     {
-      icon: <RiSecurePaymentLine className='text-6xl text-[#7049C3]' />,
+      icon: <RiSecurePaymentLine className='text-5xl text-[#7049C3]' />,
       heading: "Rapid Data Protection and Recovery",
       content: `Secure your data with Automated Backup Systems and fast disaster recovery options, ensuring minimal 
       downtime and reliable continuity.`
     },
     {
-      icon: <MdSystemUpdateAlt className='text-6xl text-[#7049C3]' />,
+      icon: <MdSystemUpdateAlt className='text-5xl text-[#7049C3]' />,
       heading: "Versatile Operating System Choices",
       content: `Customize your server environment by choosing from a wide range of Operating Systems, 
       including various Linux distributions and Windows versions, to meet your specific needs.`
     },
     {
-      icon: <LuMonitorSmartphone className='text-6xl text-[#7049C3]' />,
+      icon: <LuMonitorSmartphone className='text-5xl text-[#7049C3]' />,
       heading: "Comprehensive Monitoring and Automation",
       content: `Keep an eye on server health with Real-Time Monitoring and automate routine management tasks, 
       reducing manual intervention and enhancing efficiency.`
     },
     {
-      icon: <GrOptimize className='text-6xl text-[#7049C3]' />,
+      icon: <GrOptimize className='text-5xl text-[#7049C3]' />,
       heading: "High-Performance Computing and Machine Learning",
       content: `Leverage powerful servers optimized for Intensive Computing Tasks to run complex applications and advanced 
       Machine Learning models with high efficiency.`
