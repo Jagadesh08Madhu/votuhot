a402fe5f9252165c299c327a6523c416dfe32881
Jagadesh08Madhu <jagadesh2001kavi@gmail.com>
2025-02-25 09:08:59 +0530

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,8 +14,8 @@ export default function Home() {
         <div className='flex flex-col py-5 lg:flex-row md:px-10 '>
           {/* Left section */}
           <div className='lg:w-1/2 animate-fadeInUp flex flex-col px-2 gap-5 justify-center order-2 lg:order-1 items-center md:items-start text-gray-800'>
-            <h1 className='text-4xl text-[#2F1C6A] md:text-5xl lg:text-7xl'>Blazing-Fast <span className='text-[#673DE6]'>Hosting</span>, Engineered for Excellence</h1>
-            <p className='text-lg tracking-widest'>Experience unmatched speed and reliability with our next-generation hosting solutions. Designed for peak performance, 
+            <h1 className='text-4xl text-[#2F1C6A] md:text-4xl lg:text-6xl'>Blazing-Fast <span className='text-[#673DE6]'>Hosting</span>, Engineered for Excellence</h1>
+            <p className='text-base tracking-widest'>Experience unmatched speed and reliability with our next-generation hosting solutions. Designed for peak performance, 
               our hosting ensures lightning-fast load times, rock-solid security, and seamless scalability. Whether you're running a business 
               website or a high-traffic application, we provide the power and flexibility you need to succeed.</p>
               <div className="grid grid-cols-2 pt-10 gap-5 lg:gap-10">
