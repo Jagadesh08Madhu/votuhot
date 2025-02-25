eb5abc99b79c4be0f3f312d957512fe476f31fa3
Jagadesh08Madhu <jagadesh2001kavi@gmail.com>
2025-02-25 09:20:04 +0530

    cfdfd

--- src/components/HomeComponents/EccomContent.jsx
+++ src/components/HomeComponents/EccomContent.jsx
@@ -50,8 +50,8 @@ export default function EccomContent() {
         <div className='lg:w-1/2'><img src={eccom} alt="" /></div>
 
         <div className='lg:w-1/2 flex flex-col gap-5 lg:p-10'>
-            <h1 className='text-4xl lg:text-5xl'>BFINIT Ecommerce Custom-Built Websites</h1>
-            <p className='text-xl'>
+            <h1 className='text-3xl lg:text-4xl'>BFINIT Ecommerce Custom-Built Websites</h1>
+            <p className='text-lg'>
                 Building your business with <span className='font-bold text-[#7049C3]'>BFINIT Ecommerce Custom-Built Websites</span> means getting a 
                 tailor-made solution that perfectly aligns with your brand and business goals. Our ecommerce platforms are designed to provide 
                 seamless user experiences, high-speed performance, and <span className='font-bold text-[#7049C3]'>secure transactions</span>, ensuring that your online store operates efficiently.
